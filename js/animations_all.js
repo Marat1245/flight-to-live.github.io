cd4bc3cef4f1ecd1c5a4016d158d59a271ce7f8c
Marat1245 <69627056+Marat1245@users.noreply.github.com>
2023-03-07 11:21:38 +0500

    Заливаем файлы

---
+++ js/animations_all.js
@@ -0,0 +1,120 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('separate_01'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './js/json/separete_anima.json'
+})
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('separate_02'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './js/json/separete_anima.json'
+})
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('separate_03'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './js/json/separete_anima.json'
+})
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('separate_04'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './js/json/separete_anima.json'
+})
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('separate_05'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './js/json/separete_anima.json'
+})
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('separate_06'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './js/json/separete_anima.json'
+})
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('separate_07'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './js/json/separete_anima.json'
+})
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('separate_08'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './js/json/separete_anima.json'
+})
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('separate_09'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './js/json/separete_anima.json'
+})
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('separate_10'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './js/json/separete_anima__white.json'
+})
+
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('individual__img__json'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './js/json/flowers_03.json'
+})
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('individual__img__json-2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './js/json/flowers_03.json'
+})
+
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animation-text__01'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './js/json/text-01.json',
+
+})
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animation-text__02'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './js/json/text-02.json',
+
+})
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animation-text__03'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './js/json/text-03.json',
+
+})
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animation-text__04'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './js/json/text-04.json',
+
+})
